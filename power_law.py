7424d1314086ed2642293b621f16a2a8792016f4
Will Callender <29266431+willcallender@users.noreply.github.com>
2023-05-30 11:29:45 -0400

    power law working?
    i mean it seems to be, and it's consistent with the results of J . Zhang et al.

--- power_law.py
+++ power_law.py
@@ -15,21 +15,22 @@ t_bounds = np.array([10, 80])
 y_bounds = np.array([0, spread.max()], float)
 
 width = x_bounds[1] - x_bounds[0]
+maxL = width // 3
 # sweep through length scales
-deviations = np.zeros(width)
-num_samples = np.zeros(width)
+deviations = np.zeros(maxL)
+num_samples = np.zeros(maxL)
 for t in range(t_bounds[0], t_bounds[1]):
-    for L in range(width):
+    for L in range(1, maxL + 1):
         for i in range(x_bounds[0], x_bounds[0] + width - L):
             h = spread[t, 1, i : i + L + 1]
-            W = np.mean((h - h.mean()) ** 2)
-            deviations[L] += W
-            num_samples[L] += 1
+            W = np.mean((h - np.mean(h)) ** 2)
+            deviations[L - 1] += W
+            num_samples[L - 1] += 1
 
 def power_law(x, a, b):
     return a * x ** b
 
-x = np.arange(width)# + 1
+x = np.arange(maxL)# + 1
 y = deviations / num_samples
 
 # print(deviations / num_samples)
@@ -54,7 +55,12 @@ print("a Confidence Interval:", a_interval)
 print("b Confidence Interval:", b_interval)
 
 # Plot the fitted function
+plt.title("Power Law Fit")
+plt.ylabel("W(L)")
+plt.xlabel("L")
 plt.loglog(y)
 plt.loglog(power_law(x, *params))
-plt.legend(["Data", "Fit"])
+plt.legend(["Data", "Fit $y = {:.3f}x^{{{:.3f}}}$".format(a, b)])
+plt.text(0.1, 0.1, f"a {conf*100:.0f}% CI: {a_interval}\nb {conf*100:.0f}% CI: {b_interval}", transform=plt.gca().transAxes)
+
 plt.show()
